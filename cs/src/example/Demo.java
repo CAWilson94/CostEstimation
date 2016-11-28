0f9c1332db46ccaafadb42200440ca65fffc9ef5
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-28 17:10:12 +0000

    fitness started for min coins american money

--- cs/src/example/Demo.java
+++ cs/src/example/Demo.java
@@ -1,7 +1,17 @@
 package example;
+
 /**
  * Tutorial taken from: http://jgap.sourceforge.net/doc/tutorial.html#step2
+ * 
  * @author Charlotte Wilson
+ * 
+ *         Aims: The goal of our small program is to produce an amount of
+ *         change in American coins equal to a target amount in the fewest
+ *         possible number of coins, it seems reasonable that the measure of
+ *         fitness for a particular solution would be a combination of the
+ *         relative closeness of the amount of change it represented to the
+ *         target amount of change, and the total number of coins represented by
+ *         the solution (fewer being better).
  *
  */
 
---
+++ cs/src/example/MinimizingMakeChangeFitnessFunction.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+public class MinimizingMakeChangeFitnessFunction extends FitnessFunction {
+
+	@Override
+	protected double evaluate(IChromosome arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
