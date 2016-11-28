42793e06522406b9120e674e629b481f1c9bc6d3
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-28 17:04:21 +0000

    related classes

---
+++ cs/src/Fitness.java
@@ -0,0 +1,13 @@
+
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+public class Fitness extends FitnessFunction {
+
+	@Override
+	protected double evaluate(IChromosome arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
