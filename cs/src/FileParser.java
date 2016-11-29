32b78653631534ae6788f950eb36a2055abae0fe
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-11-29 12:25:00 +0000

    reader taking in first line of csv values

--- cs/src/FileParser.java
+++ cs/src/FileParser.java
@@ -1,7 +1,9 @@
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileParser {
 
@@ -9,6 +11,8 @@ public class FileParser {
 	 * Make a list for each column - look at how csv's would be parsed in say
 	 * Python. Emulate this.
 	 */
+
+	public List<String> boop = new ArrayList<String>();
 
 	public void file() {
 
@@ -20,9 +24,20 @@ public class FileParser {
 			br = new BufferedReader(new FileReader("china.arff.txt"));
 
 			while ((currentLine = br.readLine()) != null) {
-				System.out.println(currentLine);
+
+				String[] tokens = currentLine.split(",");
+				if (currentLine.contains("@data")) {
+					br.readLine();
+					currentLine = br.readLine();
+					System.out.println(currentLine);
+					boop.add(currentLine);
+				}
 			}
 
+			System.out.println("some shit");
+			for (String c : boop) {
+				System.out.println(c + " boop");
+			}
 			br.close();
 
 		} catch (FileNotFoundException e) {
