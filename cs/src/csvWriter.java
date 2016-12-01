93c6f5ded5bdc44c81e2a57747412e5ec7a16cd0
Charlotte Wilson <charlotteawilson.94@gmail.com>
2016-12-01 15:23:03 +0000

    csv utils for adding results

---
+++ cs/src/csvWriter.java
@@ -0,0 +1,27 @@
+import java.io.FileWriter;
+import java.util.Arrays;
+
+public class csvWriter {
+
+	public static void main(String[] args) throws Exception {
+
+		String csvFile = "abc.csv";
+		FileWriter writer = new FileWriter(csvFile);
+
+		CSVUtils.writeLine(writer, Arrays.asList("a", "b", "c", "d"));
+
+		// custom separator + quote
+		CSVUtils.writeLine(writer, Arrays.asList("aaa", "bb,b", "cc,c"), ',', '"');
+
+		// custom separator + quote
+		CSVUtils.writeLine(writer, Arrays.asList("aaa", "bbb", "cc,c"), '|', '\'');
+
+		// double-quotes
+		CSVUtils.writeLine(writer, Arrays.asList("aaa", "bbb", "cc\"c"));
+
+		writer.flush();
+		writer.close();
+
+	}
+
+}
